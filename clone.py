e0a31e72a069b873ff6880ae6aef812b9735ad84
Yuriy Dybskiy <yuriy@dybskiy.com>
2017-08-16 01:03:09 -0400

    Add lambda and data augmentation

--- clone.py
+++ clone.py
@@ -8,28 +8,31 @@ with open('data/driving_log.csv') as csvfile:
     for line in reader:
         lines.append(line)
 
-images = []
-measurements = []
+images, measurements = [], []
 for line in lines:
     source_path = line[0]
     filename = source_path.split('/')[-1]
-    current_path = '../data/IMG/' + filename
+    current_path = './data/IMG/' + filename
     image = cv2.imread(current_path)
     images.append(image)
     measurement = float(line[3])
     measurements.append(measurement)
+    # augment data with a flipped version
+    images.append(cv2.flip(image, 1))
+    measurements.append(measurement * -1.0)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
 
 from keras.models import Sequential
-from keras.layers import Flatten, Dense
+from keras.layers import Flatten, Dense, Lambda
 
 model = Sequential()
-model.add(Flatten(input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=7)
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=1)
 
 model.save('model.h5')

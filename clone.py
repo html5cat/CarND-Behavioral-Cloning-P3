f19415ab41afcc0515ff5401b4b0048360310970
Yuriy Dybskiy <yuriy@dybskiy.com>
2017-08-15 22:40:54 -0400

    typo

--- clone.py
+++ clone.py
@@ -26,7 +26,7 @@ from keras.models import Sequential
 from keras.layers import Flatten, Dense
 
 model = Sequential()
-model.add(Flatten(imput_shape=(160,320,3)))
+model.add(Flatten(input_shape=(160,320,3)))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')

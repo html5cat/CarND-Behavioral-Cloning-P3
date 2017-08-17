806398a8a09dbdce6385051334d5b7d4a3dac05c
Yuriy Dybskiy <yuriy@dybskiy.com>
2017-08-16 23:33:04 -0400

    Update model

--- clone.py
+++ clone.py
@@ -1,6 +1,8 @@
 import cv2
 import csv
 import numpy as np
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
 
 lines = []
 with open('data/driving_log.csv') as csvfile:
@@ -9,26 +11,32 @@ with open('data/driving_log.csv') as csvfile:
         lines.append(line)
 
 images, measurements = [], []
+correction_dict = {
+    0 : 0,
+    1: 0.2,
+    2: -0.2
+}
+
 for line in lines:
-    source_path = line[0]
-    filename = source_path.split('/')[-1]
-    current_path = './data/IMG/' + filename
-    image = cv2.imread(current_path)
-    images.append(image)
-    measurement = float(line[3])
-    measurements.append(measurement)
-    # augment data with a flipped version
-    images.append(cv2.flip(image, 1))
-    measurements.append(measurement * -1.0)
+    for i in range(3):
+        source_path = line[i]
+        filename = source_path.split('/')[-1]
+        current_path = './data/IMG/' + filename
+        image = cv2.imread(current_path)
+        images.append(image)
+        measurement = float(line[3]) + correction_dict[i]
+        measurements.append(measurement)
+        # augment data with a flipped version
+        images.append(cv2.flip(image, 1))
+        measurements.append(measurement * -1.0)
 
 X_train = np.array(images)
 y_train = np.array(measurements)
 
-from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda
 
 model = Sequential()
 model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Cropping2D(cropping=((70,25), (0,0))))
 model.add(Flatten())
 model.add(Dense(1))
 

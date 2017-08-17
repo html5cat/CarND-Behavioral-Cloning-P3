6c5cb3ee621b81d292f96410c6a0c1dabd9b15c1
Yuriy Dybskiy <yuriy@dybskiy.com>
2017-08-17 15:47:52 -0400

    Refactor to use generators

--- clone.py
+++ clone.py
@@ -1,45 +1,65 @@
+import os
 import cv2
 import csv
 import numpy as np
+import sklearn
+from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 
-lines = []
+samples = []
 with open('data/driving_log.csv') as csvfile:
     reader = csv.reader(csvfile)
     for line in reader:
-        lines.append(line)
+        samples.append(line)
 
-images, measurements = [], []
+train_samples, validation_samples = train_test_split(samples, test_size = 0.2) 
 correction_dict = {
     0 : 0,
     1: 0.2,
     2: -0.2
 }
 
-for line in lines:
-    for i in range(3):
-        source_path = line[i]
-        filename = source_path.split('/')[-1]
-        current_path = './data/IMG/' + filename
-        image = cv2.imread(current_path)
-        images.append(image)
-        measurement = float(line[3]) + correction_dict[i]
-        measurements.append(measurement)
-        # augment data with a flipped version
-        images.append(cv2.flip(image, 1))
-        measurements.append(measurement * -1.0)
+def generator(samples, batch_size = 32):
+    num_samples = len(samples)
+    while 1:
+        sklearn.utils.shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset : offset + batch_size]
 
-X_train = np.array(images)
-y_train = np.array(measurements)
+            images, angles = [], []
+            for batch_sample in batch_samples:
+                for i in range(3):
+                    file_path = './data/IMG/' + batch_sample[i].split('/')[-1]
+                    image = cv2.imread(file_path)
+                    angle = float(line[3]) + correction_dict[i]
+                    images.append(image)
+                    angles.append(angle)
+                    images.append(cv2.flip(image, 1))
+                    angles.append(angle * -1.0)
+
+            X_train = np.array(images)
+            y_train = np.array(angles)
+
+            yield sklearn.utils.shuffle(X_train, y_train)
+
+train_generator = generator(train_samples, batch_size = 32)
+validation_generator = generator(validation_samples, batch_size = 32)
+
+ch, row, col = 3, 160, 320
 
 model = Sequential()
-model.add(Lambda(lambda x: x / 255.0 - 0.5, input_shape=(160,320,3)))
+model.add(Lambda(lambda x: x / 255.0 - 0.5, 
+                    input_shape=(row, col, ch)))
 model.add(Cropping2D(cropping=((70,25), (0,0))))
 model.add(Flatten())
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=1)
-
+model.fit_generator(train_generator, 
+                    samples_per_epoch = len(train_samples) * 6,
+                    validation_data = validation_generator, 
+                    nb_val_samples = len(validation_samples), 
+                    nb_epoch = 1)
+                    
 model.save('model.h5')

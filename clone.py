93ee157f46479daf2cea54475850e112e07b995b
Yuriy Dybskiy <yuriy@dybskiy.com>
2017-08-21 22:06:10 -0400

    Add better net

--- clone.py
+++ clone.py
@@ -5,7 +5,7 @@ import numpy as np
 import sklearn
 from sklearn.model_selection import train_test_split
 from keras.models import Sequential
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Convolution2D
 
 epochs = 1
 correction_dict = {
@@ -55,7 +55,14 @@ model = Sequential()
 model.add(Lambda(lambda x: (x / 255.0) - 0.5, 
                     input_shape=(row, col, ch)))
 model.add(Cropping2D(cropping=((70,25), (0,0))))
+model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Convolution2D(64,3,3,activation="relu"))
 model.add(Flatten())
+model.add(Dense(100))
+model.add(Dense(50))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
